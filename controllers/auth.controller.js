3a2cab5820111c786ef9142a45cb18bb4cd5ea00
Dineth Dilshan <E2145112@bit.uom.lk>
2024-02-24 00:28:04 +0530

    Main Function User

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
@@ -7,7 +9,7 @@ export const signup = async (req, res) => {
     if (password !== confirmPassword) {
       return res.status(400).json({ error: "Passwords don't match" });
     }
-    console.log(fullName);
+
     const user = await User.findOne({ username });
 
     if (user) {
@@ -30,26 +32,54 @@ export const signup = async (req, res) => {
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
-
-    await newUser.save();
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+    if (newUser) {
+      await newUser.save();
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    }
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-export const login = (req, res) => {
-  console.log("Login User");
-  res.send("hello World log in");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const IsPassowrdCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !IsPassowrdCorrect) {
+      return res.status(400).json({ error: "Invalid Username Or Password" });
+    }
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      password: user.password,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in Log in controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-  console.log("Log out User");
-  res.send("hello World log out ");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Log Out Successfully" });
+  } catch (error) {
+    console.log("Error in log out COntroller", error.message);
+    res.status(500).json({ error: "Internal Server Eroor" });
+  }
 };
